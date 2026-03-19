be019073610a46b06df80348252e87b84c55612a
Smitha Rajappa <116454538+smitharajappa@users.noreply.github.com>
2026-03-18 20:14:58 -0500

    Adjust base URL based on GITHUB_ACTIONS environment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: '/rewardforge/',
+  base: process.env.GITHUB_ACTIONS ? '/rewardforge/' : '/',
   server: {
     host: "::",
     port: 8080,
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === "development" && componentTagger()
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
